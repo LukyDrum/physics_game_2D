35b34e548b64bd73115781e61ceeb92d186b5415
Luky Drum <lukbubi12@gmail.com>
2025-04-08 10:08:39 +0200

    Does it work now?

--- game_macros/src/lib.rs
+++ game_macros/src/lib.rs
@@ -1,7 +1,11 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(UIEdit)]
 pub fn derive_ui_edit(tokens: TokenStream) -> TokenStream {
-    
+    let input = parse_macro_input!(tokens as DeriveInput);
+
+    quote! {}.into()
 }

37d2fdc776f8347323dc44247855ebb1e6824553
LukyDrum <lukbubi12@gmail.com>
2025-05-03 13:05:22 +0200

    Periodicly check for new save files

--- game/src/game/ui/components/saves_loads.rs
+++ game/src/game/ui/components/saves_loads.rs
@@ -1,4 +1,8 @@
 use std::collections::LinkedList;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Once, RwLock};
+use std::thread::{self, JoinHandle};
+use std::time::Duration;
 
 use macroquad::text::draw_text;
 use macroquad::ui::root_ui;
@@ -13,9 +17,13 @@ use crate::{
     math::{v2, Vector2},
 };
 
+const RECHECK_TIME: u64 = 3;
+
 pub struct SavesLoads {
     pub action: SaveLoadAction,
-    saves: LinkedList<String>,
+    saves: Arc<RwLock<LinkedList<String>>>,
+    _check_handle: JoinHandle<()>,
+    end_of_checks_flag: Arc<AtomicBool>,
     pub save_file_name: String,
     pub taken_input: bool,
 }
@@ -28,15 +36,53 @@ pub enum SaveLoadAction {
 
 impl Default for SavesLoads {
     fn default() -> Self {
-        SavesLoads {
-            action: SaveLoadAction::Nothing,
-            saves: save_load::list_saves()
+        let saves = Arc::new(RwLock::new(
+            save_load::list_saves()
                 .iter()
                 .filter_map(|s| s.strip_suffix(".json").map(|s| s.to_owned()))
                 .collect(),
+        ));
+
+        let end_of_checks = Arc::new(AtomicBool::new(false));
+        let handle = {
+            let saves = saves.clone();
+            let end_of_checks = end_of_checks.clone();
+            thread::spawn(move || {
+                periodicly_check_save_files(saves, end_of_checks);
+            })
+        };
+
+        SavesLoads {
+            action: SaveLoadAction::Nothing,
+            saves,
+            _check_handle: handle,
+            end_of_checks_flag: end_of_checks,
             save_file_name: "save-1".to_owned(),
             taken_input: false,
         }
+    }
+}
+
+impl Drop for SavesLoads {
+    fn drop(&mut self) {
+        self.end_of_checks_flag.store(true, Ordering::Relaxed);
+    }
+}
+
+fn periodicly_check_save_files(
+    saves: Arc<RwLock<LinkedList<String>>>,
+    end_of_checks: Arc<AtomicBool>,
+) -> () {
+    loop {
+        if end_of_checks.load(Ordering::Relaxed) {
+            return;
+        }
+
+        thread::sleep(Duration::from_secs(RECHECK_TIME));
+
+        let new_saves = save_load::list_saves();
+        let mut write = saves.write().unwrap();
+        *write = new_saves;
     }
 }
 
@@ -71,7 +117,8 @@ impl UIComponent for SavesLoads {
         );
 
         offset += v2!(0.0, 50.0);
-        for save in &self.saves {
+        let read = self.saves.read().unwrap();
+        for save in &*read {
             if Button::new(save.as_str())
                 .size(v2!(100.0, 25.0).as_mq())
                 .position(offset.as_mq())
